8db599f0ef4232348fb6070cca8337cd9442d2c2
Mario Silva <mariosilva.81@icloud.com>
2023-11-21 10:07:21 -0300

    fix: modified app and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import './App.css';
+import { GlobalStyle } from './styles/globalStyles';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { RoutesMain } from './routes/RoutesMain';
+import { useClientContext } from './providers/ClientContext';
+import { StyledLoaderContainer } from './styles/grid';
+import { HashLoader } from 'react-spinners';
 
-function App() {
-  return <h1>teste</h1>;
-}
+const App = () => {
+  const { globalLoading } = useClientContext();
+
+  return (
+    <>
+      {globalLoading ? (
+        <StyledLoaderContainer>
+          <HashLoader color="#ff427f" />
+        </StyledLoaderContainer>
+      ) : (
+        <RoutesMain />
+      )}
+      <GlobalStyle />
+      <ToastContainer />
+    </>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ClientContextProvider } from './providers/ClientContext';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ClientContextProvider>
+        <App />
+      </ClientContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
