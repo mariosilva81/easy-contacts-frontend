625b64ae105fd2b069072d05d89a1cb4c37430cb
Mario Silva <mariosilva.81@icloud.com>
2023-11-23 15:29:43 -0300

    fix: modified base url at api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://fullstack-project-eh5a.onrender.com',
   baseURL: 'http://localhost:3000',
   timeout: 15000,
 });
